9ad3c6713fb861f26911ae38e8daa0a3107a1fce
andrewsf <andrewsf@andrewsf>
2012-04-05 18:06:17 -0700

    537004

--- js-sdk/all.beautified.js
+++ js-sdk/all.beautified.js
@@ -1,4 +1,4 @@
-/*1333652448,169923946,JIT Construction: v536612,en_US*/
+/*1333671012,169938301,JIT Construction: v537004,en_US*/
 
 var FB;
 if (!FB) {
@@ -174,6 +174,79 @@ if (!FB) {
                 return c;
             }
         });
+        FB.provide('UA', {
+            ie: function() {
+                return FB.UA._populate() || this._ie;
+            },
+            firefox: function() {
+                return FB.UA._populate() || this._firefox;
+            },
+            opera: function() {
+                return FB.UA._populate() || this._opera;
+            },
+            safari: function() {
+                return FB.UA._populate() || this._safari;
+            },
+            chrome: function() {
+                return FB.UA._populate() || this._chrome;
+            },
+            windows: function() {
+                return FB.UA._populate() || this._windows;
+            },
+            osx: function() {
+                return FB.UA._populate() || this._osx;
+            },
+            linux: function() {
+                return FB.UA._populate() || this._linux;
+            },
+            ios: function() {
+                FB.UA._populate();
+                return FB.UA.mobile() && this._ios;
+            },
+            mobile: function() {
+                FB.UA._populate();
+                return !FB._inCanvas && this._mobile;
+            },
+            nativeApp: function() {
+                return FB.UA.mobile() && navigator.userAgent.match(/FBAN\/\w+;/i);
+            },
+            android: function() {
+                FB.UA._populate();
+                return FB.UA.mobile() && this._android;
+            },
+            iPad: function() {
+                FB.UA._populate();
+                return FB.UA.mobile() && this._iPad;
+            },
+            _populated: false,
+            _populate: function() {
+                if (FB.UA._populated) return;
+                FB.UA._populated = true;
+                var a = /(?:MSIE.(\d+\.\d+))|(?:(?:Firefox|GranParadiso|Iceweasel).(\d+\.\d+))|(?:Opera(?:.+Version.|.)(\d+\.\d+))|(?:AppleWebKit.(\d+(?:\.\d+)?))/.exec(navigator.userAgent),
+                    b = /(Mac OS X)|(Windows)|(Linux)/.exec(navigator.userAgent),
+                    c = /\b(iPhone|iP[ao]d)/.exec(navigator.userAgent);
+                FB.UA._iPad = /\b(iPad)/.exec(navigator.userAgent);
+                FB.UA._android = navigator.userAgent.match(/Android/i);
+                FB.UA._mobile = c || FB.UA._android || navigator.userAgent.match(/Mobile/i);
+                if (a) {
+                    FB.UA._ie = a[1] ? parseFloat(a[1]) : NaN;
+                    if (FB.UA._ie >= 8 && !window.HTMLCollection) FB.UA._ie = 7;
+                    FB.UA._firefox = a[2] ? parseFloat(a[2]) : NaN;
+                    FB.UA._opera = a[3] ? parseFloat(a[3]) : NaN;
+                    FB.UA._safari = a[4] ? parseFloat(a[4]) : NaN;
+                    if (FB.UA._safari) {
+                        a = /(?:Chrome\/(\d+\.\d+))/.exec(navigator.userAgent);
+                        FB.UA._chrome = a && a[1] ? parseFloat(a[1]) : NaN;
+                    } else FB.UA._chrome = NaN;
+                } else FB.UA._ie = FB.UA._firefox = FB.UA._opera = FB.UA._chrome = FB.UA._safari = NaN;
+                if (b) {
+                    FB.UA._osx = !! b[1];
+                    FB.UA._windows = !! b[2];
+                    FB.UA._linux = !! b[3];
+                } else FB.UA._osx = FB.UA._windows = FB.UA._linux = false;
+                FB.UA._ios = c;
+            }
+        });
         FB.provide('Content', {
             _root: null,
             _hiddenRoot: null,
@@ -181,10 +254,20 @@ if (!FB) {
             append: function(a, b) {
                 if (!b) if (!FB.Content._root) {
                     FB.Content._root = b = FB.$('fb-root');
-                    if (!b) {
-                        FB.log('The "fb-root" div has not been created.');
+                    if (!b) if (FB.XD.sendToFacebook) {
+                        FB.log('The "fb-root" div has not been created, auto-creating');
+                        FB.Content._root = b = document.createElement('div');
+                        b.id = 'fb-root';
+                        if (FB.UA.ie() || !document.body) {
+                            FB.Dom.ready(function() {
+                                document.body.appendChild(b);
+                            });
+                        } else document.body.appendChild(b);
+                    } else {
+                        FB.log('The "fb-root" div has not been created');
                         return;
-                    } else b.className += ' fb_reset';
+                    }
+                    b.className += ' fb_reset';
                 } else b = FB.Content._root;
                 if (typeof a == 'string') {
                     var c = document.createElement('div');
@@ -1636,12 +1719,12 @@ if (!FB) {
                 }
             }
             var ba = function() {
-                    var ea = document.body.appendChild(document.createElement("form")),
+                    var ea = document.createElement("form"),
                         fa = ea.appendChild(document.createElement("input")),
                         ga;
                     fa.name = m();
                     ga = fa !== ea.elements[fa.name];
-                    document.body.removeChild(ea);
+                    ea = fa = null;
                     ba = function() {
                         return ga;
                     };
@@ -1749,79 +1832,6 @@ if (!FB) {
             var e = c('SDKConfig');
             if (!e.legacy) FB.XD = b('SDK_XD');
         }, 3);
-        FB.provide('UA', {
-            ie: function() {
-                return FB.UA._populate() || this._ie;
-            },
-            firefox: function() {
-                return FB.UA._populate() || this._firefox;
-            },
-            opera: function() {
-                return FB.UA._populate() || this._opera;
-            },
-            safari: function() {
-                return FB.UA._populate() || this._safari;
-            },
-            chrome: function() {
-                return FB.UA._populate() || this._chrome;
-            },
-            windows: function() {
-                return FB.UA._populate() || this._windows;
-            },
-            osx: function() {
-                return FB.UA._populate() || this._osx;
-            },
-            linux: function() {
-                return FB.UA._populate() || this._linux;
-            },
-            ios: function() {
-                FB.UA._populate();
-                return FB.UA.mobile() && this._ios;
-            },
-            mobile: function() {
-                FB.UA._populate();
-                return !FB._inCanvas && this._mobile;
-            },
-            nativeApp: function() {
-                return FB.UA.mobile() && navigator.userAgent.match(/FBAN\/\w+;/i);
-            },
-            android: function() {
-                FB.UA._populate();
-                return FB.UA.mobile() && this._android;
-            },
-            iPad: function() {
-                FB.UA._populate();
-                return FB.UA.mobile() && this._iPad;
-            },
-            _populated: false,
-            _populate: function() {
-                if (FB.UA._populated) return;
-                FB.UA._populated = true;
-                var a = /(?:MSIE.(\d+\.\d+))|(?:(?:Firefox|GranParadiso|Iceweasel).(\d+\.\d+))|(?:Opera(?:.+Version.|.)(\d+\.\d+))|(?:AppleWebKit.(\d+(?:\.\d+)?))/.exec(navigator.userAgent),
-                    b = /(Mac OS X)|(Windows)|(Linux)/.exec(navigator.userAgent),
-                    c = /\b(iPhone|iP[ao]d)/.exec(navigator.userAgent);
-                FB.UA._iPad = /\b(iPad)/.exec(navigator.userAgent);
-                FB.UA._android = navigator.userAgent.match(/Android/i);
-                FB.UA._mobile = c || FB.UA._android || navigator.userAgent.match(/Mobile/i);
-                if (a) {
-                    FB.UA._ie = a[1] ? parseFloat(a[1]) : NaN;
-                    if (FB.UA._ie >= 8 && !window.HTMLCollection) FB.UA._ie = 7;
-                    FB.UA._firefox = a[2] ? parseFloat(a[2]) : NaN;
-                    FB.UA._opera = a[3] ? parseFloat(a[3]) : NaN;
-                    FB.UA._safari = a[4] ? parseFloat(a[4]) : NaN;
-                    if (FB.UA._safari) {
-                        a = /(?:Chrome\/(\d+\.\d+))/.exec(navigator.userAgent);
-                        FB.UA._chrome = a && a[1] ? parseFloat(a[1]) : NaN;
-                    } else FB.UA._chrome = NaN;
-                } else FB.UA._ie = FB.UA._firefox = FB.UA._opera = FB.UA._chrome = FB.UA._safari = NaN;
-                if (b) {
-                    FB.UA._osx = !! b[1];
-                    FB.UA._windows = !! b[2];
-                    FB.UA._linux = !! b[3];
-                } else FB.UA._osx = FB.UA._windows = FB.UA._linux = false;
-                FB.UA._ios = c;
-            }
-        });
         FB.provide('Arbiter', {
             _canvasProxyUrl: 'connect/canvas_proxy.php',
             BEHAVIOR_EVENT: 'e',
