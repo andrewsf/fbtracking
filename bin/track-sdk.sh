e91ff2409baf46ca8d42a37ab2e5e062afdfba38
andrewsf <andrewsf@andrewsf>
2013-10-11 16:25:01 -0700

    Add script that tracks the JS SDK

---
+++ bin/track-sdk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="`dirname \"$0\"`/.."
+pushd $DIR/js-sdk/
+wget connect.facebook.net/en_US/all/debug.js -O all/debug.js;
+wget connect.facebook.net/en_US/all.js -O all.js;
+$DIR/bin/js-beautify/python/js-beautify all.js > all.beautified.js;
+head -1 all.beautified.js | cut -d',' -f3 | cut -d'v' -f2 > version;
+test -n "$(git diff --numstat HEAD version)" && (cat version | xargs -I {} git commit -am '{}');
+git push
+popd
+
